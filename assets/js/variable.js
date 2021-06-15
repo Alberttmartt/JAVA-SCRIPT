1c004c2c5325422272678d86dbf188ac44650526
Alberto Martinez <alberto.martinez08@gmail.com>
2021-06-15 18:22:24 -0500

    JAVA-SCRIPT

---
+++ assets/js/variable.js
@@ -0,0 +1,60 @@
+var variable1 = 'soy la variable 1';
+console.log(variable1);
+variable1 = 1212;
+console.log(variable1);
+
+const variable2 ='Variable 2';
+console.log(variable2);
+/* variable2= 10;
+console.log(variable2); */
+
+let nueva_variable2='mi nueva variable';
+console.log(nueva_variable2);
+
+function vartest() {
+    var x= 31;
+    if (true) {
+        var x = 71; //
+        console.log(x); //71 Uriel
+    }
+    console.log(x); // 31 Gabi
+    }
+
+/* vartest();*/
+
+  function vartest () {
+    var x = 31;
+    if (true) {
+        var x = 71; //
+        console.log(x); //71 Gustavo
+    }
+    console.log(x); // 31 Miguel
+    }
+    
+    
+    const nuevaFuncion = () => {
+        var x= 31;
+    if (true) {
+        var x = 71; //
+        console.log(x); //71 Uriel
+    }
+    console.log(x); // 31 Gabi
+ }
+  
+
+    nuevaFuncion();
+    
+    /* letTest(); */
+    
+    console.log(nueva_variable2);
+
+    const nombre = 'carlos';
+    const apellidos = 'jesus leon';
+
+    console.log('Hola mi nombre es ${nombre} y mis apellidos son ${apellidos}');
+
+    const arreglo = [1, 2, 3, 4, 5];
+    console.log(arreglo);
+
+
+
